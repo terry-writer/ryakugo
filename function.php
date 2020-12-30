f79d82ad6846e92662e9e4ed010ff5682649b6dd
Hiroto-Oohashi <byebye0713@gmail.com>
2020-12-30 12:34:37 +0900

    functionの配列残す

--- function.php
+++ function.php
@@ -1,28 +1,10 @@
 <?php
 
-$bunsyou = 'This is a pen';
-$pieces = explode(" ", $bunsyou);
-
-$i = 0;
-while(empty($pieces[$i]) === true){
-    $test = substr($pieces[$i], 0, 1);
-    echo $test;
-    $i ++;
+function ryakugo($bunsyou){
+    $array = explode(" ", $bunsyou);
+    foreach ($array as $value) {
+        $value = substr($value, 0, 1);
+        print_r($value);
+    }
 }
-
-
-$test2 = substr($pieces[0], 0, 1);
-echo $test2;
-
-
-// if(empty($pieces[4])){
-//     echo '配列はからです';
-// }else{
-//     echo '何か入ってます';
-// }
-
-// var_dump(empty($pieces[4]));
-// $test = substr($pieces[0], 0, 1);
-// echo $test;
-
 
--- test.php
+++ test.php
@@ -1,2 +1,57 @@
 <?php
-echo 'hello';
+ require ('function.php');
+
+ $bunsyou = 'as soon as possible';
+
+ ryakugo($bunsyou);
+
+ echo '<br>';
+
+ $bunsyou = 'best friend forever';
+
+ ryakugo($bunsyou);
+
+
+ echo '<br>';
+
+ $bunsyou = 'This is my pen';
+
+ ryakugo($bunsyou);
+
+
+ echo '<br>';
+
+ $bunsyou = 'My name is cat';
+
+ ryakugo($bunsyou);
+
+
+ echo '<br>';
+
+ $bunsyou = 'no music no life';
+
+ ryakugo($bunsyou);
+
+ echo '<br>';
+
+ $answers = [
+    "as soon as possible" => "asap",
+    'best friend forever' => 'bff',
+    'This is my pen' => 'Timp',
+    'My name is cat' => 'Mnic',
+    'no music no life' => 'nmnl',
+];
+
+
+
+
+// foreach ($answers as $answer => $expect) {
+//     echo ' 略す前 = '.  $answer, ' 略語 = '.  ($expect ? 'true' : 'false');
+
+//     if (ryakugo($bunsyou) === $expect) {
+//         echo ' OK 一致';
+//     } else {
+//         echo ' NG 不一致';
+//     }
+//     echo '<br>', PHP_EOL;
+// }
