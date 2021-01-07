6dfdb815a5b45b0384128d711e400d5250166c49
Hiroto-Oohashi <byebye0713@gmail.com>
2021-01-07 18:22:11 +0900

    いろいろ試行錯誤

--- function.php
+++ function.php
@@ -1,10 +1,52 @@
 <?php
+//test_array patern3
+// $arr =[];
+// function ryakugo($bunsyou){
+//     $array = explode(" ", $bunsyou);
+//     foreach ($array as $value) {
+//         $arr[] = substr($value, 0, 1);
+//     }
+//     return $arr;
+// }
 
+//test1 patern1
 function ryakugo($bunsyou){
+    $arr =[];
     $array = explode(" ", $bunsyou);
     foreach ($array as $value) {
-        $value = substr($value, 0, 1);
-        print_r($value);
+        $arr[] = substr($value, 0, 1);
     }
+    return implode($arr);
 }
+
+$test = ryakugo('this is a pen');
+var_dump($test);
+
+// $a = array(1, 2, 3, 17);
+
+// $i = 1; /* 説明用 */
+
+// foreach ($a as $v) {
+//     echo "\$a[$i] => $v.\n";
+//     $i++;
+//}
+
+
+
+
+
+
+//test patern2
+// function ryakugo($bunsyou){
+//     $array = explode(" ", $bunsyou);
+//     foreach ($array as $value) {
+//         if($value !== null){
+//             $value = substr($value, 0, 1);
+//             $value . $value;
+//         }else{
+//             $value = 'null test';
+//             return $value;
+//         }        
+//     }
+// }
 
